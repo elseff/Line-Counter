3b37ac3eb0c809fe88779f234f89972b59605d73
Данила <danilapetrov522@gmail.com>
2022-10-20 16:48:33 +0400

    init commit

---
+++ src/main/java/com/elseff/project/Counter.java
@@ -0,0 +1,45 @@
+package com.elseff.project;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Arrays;
+import java.util.Objects;
+import java.util.Scanner;
+
+public class Counter {
+    private static int total = 0;
+
+    public static void main(String[] args){
+        String pathToProject = "D:\\dev\\LineCounter"; // HERE PATH TO YOUR JAVA PROJECT
+        File project = new File(pathToProject);
+
+        count(project);
+
+        System.out.printf("\nProject %s has total %d lines", project, total);
+    }
+
+    public static void count(File file) {
+        if (file.isFile())
+            total+=countLines(file);
+        else if (file.isDirectory())
+            Arrays.stream(Objects.requireNonNull(file.listFiles())).forEach(Counter::count);
+    }
+
+    public static int countLines(File file) {
+        int count = 0;
+        Scanner scanner;
+        try {
+            if(file.getName().endsWith(".java")) {
+                scanner = new Scanner(file);
+                while (scanner.hasNextLine()) {
+                    scanner.nextLine();
+                    count++;
+                }
+                System.out.printf("%s has %d lines\n", file.getName(), count);
+            }
+        } catch (FileNotFoundException exception) {
+            exception.printStackTrace();
+        }
+        return count;
+    }
+}
